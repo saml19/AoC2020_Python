b96b50ffef429e9b795cca39c0b6c5a6f71a26de
Sam lewis-LIm <sll537@york.ac.uk>
2020-12-08 17:17:09 +0000

    part 2 day 2

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,7 +1,6 @@
 import re
 with open('input.txt', 'r') as reader:
     input_data = reader.read().strip('\n').split('\n')
-    print(input_data)
 
 
 def valid_password(password) -> bool:
@@ -9,10 +8,25 @@ def valid_password(password) -> bool:
     if int(data_dict["min"]) <= data_dict["password"].count(data_dict["letter"]) <= int(data_dict["max"]):
         return True
 
+def valid_password_part_2(password) -> bool:
+    data_dict = re.match("(?P<pos1>\d+)-(?P<pos2>\d+) (?P<letter>\w): (?P<password>\w+)", password).groupdict()
+    letter = data_dict["letter"]
+    pos1 = int(data_dict["pos1"])
+    pos2 = int(data_dict["pos2"])
+    pos1_match = letter == data_dict["password"][pos1-1]
+    pos2_match = letter == data_dict["password"][pos2-1]
+    return pos2_match ^ pos1_match
+
 
 valid_count = 0
 for password in input_data:
     if valid_password(password):
         valid_count += 1
 
-print(valid_count)
+valid_count_2 = 0
+for password in input_data:
+    if valid_password_part_2(password):
+        valid_count_2 += 1
+
+print("part 1 valid count", valid_count)
+print("part 2 valid count", valid_count_2)
